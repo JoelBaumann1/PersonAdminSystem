24238de3d50e1ad8e3a1813f9035f74fc8b8d887
JoelBaumann1 <100359477+JoelBaumann1@users.noreply.github.com>
2023-11-11 10:24:24 +0100

    Add project files.

---
+++ PersonAdmin/PersonAdmin.cs
@@ -0,0 +1,15 @@
+﻿using PersonAdminLib;
+using System.Reflection;    
+namespace PersonAdmin
+{
+    internal class PersonAdmin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"My first C# Program: {Assembly.GetExecutingAssembly().GetName().Version}");
+            Person person = new Person("Jack", "Smith");
+            Console.WriteLine($"Firstname: {person.Firstname} Lastname: {person.Surname}");
+
+        }
+    }
+}
---
+++ PersonAdminLib/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonAdminLib
+{
+    public class Person
+    {
+        public string Firstname { get; }
+        public string Surname { get; }
+
+        public Person(string firstname, string surname)
+        {
+            Firstname = firstname;
+            Surname = surname;
+        }
+    }
+}
